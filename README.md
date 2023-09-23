900874737a489c3f6dac9a65427dd8be768489d8
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-09-23 18:08:49 +0200

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/WKBLhfd
+https://ibb.co/DKrHDS1
+https://ibb.co/yp1nMc1
+https://ibb.co/PgsJsr6
---
+++ index.js
@@ -0,0 +1,41 @@
+import yargs from "yargs";
+import { program } from "commander";
+import * as contactsOptions from "./contacts.js";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    try {
+        switch (action) {
+            case "list":
+                const contactsList = await contactsOptions.listContacts();
+                return console.log(contactsList);
+            case "get":
+                const contactById = await contactsOptions.getContactById(id);
+                return console.log(contactById);
+            case "add":
+                const newContact = await contactsOptions.addContact(name, email, phone);
+                return console.log(newContact);
+
+            case "remove":
+                const removedContact = await contactsOptions.removeContact(id);
+                return console.log(removedContact);
+
+            default:
+                console.warn("\x1B[31m Unknown action type!");
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
